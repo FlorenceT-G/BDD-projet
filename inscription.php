30a5c2cd59fb06563e6f419ec10c7c71c2fb0031
FlorenceT-G <57944788+FlorenceT-G@users.noreply.github.com>
2019-11-21 13:00:54 +0100

    cryptage mdp
    Tentative d'implémentation d'un mot de passe crypté > pas encore testé !
    
    +implémentation de quelques commentaires

--- inscription.php
+++ inscription.php
@@ -5,7 +5,6 @@
 	{
 		//keep track validation errors
 		$pseudoErreur = null;
-		$mdpErreur = null;
 		$prenomErreur = null;
 		$nomErreur = null;
 		$emailErreur = null;
@@ -13,10 +12,12 @@
 		$jourErreur = null;
 		$moisErreur = null;
 		$anneeErreur = null;
+		
+		$mdpErreur = null;
+		$repeatmdp = null;
 	
 		// keep track post values
 		$pseudo = $_POST['pseudo'];
-		$mdp = $_POST['mdp'];
 		$nom = $_POST['nom'];
 		$prenom = $_POST['prenom'];
 		$email = $_POST['email'];
@@ -25,18 +26,15 @@
 		$mois = $_POST['mois'];
 		$annee = $_POST['annee'];
 		
+		$mdp = $_POST['mdp'];
+		$repeatmdp = $_POST['repeatmdp'];
+		
 		// validate input
 		$valid = true;
 
 		if (empty($pseudo)) 
 		{
 			$pseudoErreur = 'Veuillez entrer un pseudo';
-			$valid = false;
-		}
-		
-		if (empty($mdp))
-		{
-			$mdpErreur = 'Veuillez entrer un mot de passe.';
 			$valid = false;
 		}
 
@@ -87,23 +85,51 @@
 			$valid = false;
 		}
 		
-		//insert data
-		if ($valid) 
+		if (empty($mdp))
 		{
-			$naissance = "$annee-$mois-$jour";
-			$d_inscription = date("Y-m-d");
+			$mdpErreur = 'Veuillez entrer un mot de passe.';
+			$valid = false;
+		}
 		
-			$base = mysqli_connect(MYHOST, MYUSER, MYPASS, DBNAME); 					   
-			$sql = 	"INSERT INTO `personne`(pseudo, mail, naissance, inscription, nom, prenom, residence, mot_de_passe) VALUES ('$pseudo', '$email', '$naissance', '$d_inscription', '$nom', '$prenom','$residence', '$mdp');";	
-			$result=mysqli_query($base,$sql);	
+		if (empty($repeatmdp))
+		{
+			$repeatmdpErreur = 'Veuillez répéter votre mot de passe.';
+			$valid = false;
+		}
+		
+		
+		//insert data
+		if ($valid) //si aucun champ ne se trouve vide alors on peut passer au reste
+		{
+			$naissance = "$annee-$mois-$jour"; //transformation de la date entrée par l'utilisateur en un format accepté par MySQL
+			$d_inscription = date("Y-m-d"); //récupère la date à laquelle l'utilisateur s'inscrit
 			
-			if(!$result)
-			{	
-				 echo("Error description : ".mysqli_error($base));	
-			}	
+			if(strlen($mdp)>=6) //on vérifie que le mot de passe fasse au moins 6 caractères > sécurité
+			{
+				if($mdp == $repeatmdp) //si mot de passe répété est équivalent au mot de passe saisi initialement
+				{	
+					$mdp = md5($mdp); //cryptage du mot de passe
+					$base = mysqli_connect(MYHOST, MYUSER, MYPASS, DBNAME); //requête de connexion à la base de donnée					   
+					$sql = 	"INSERT INTO `personne`(pseudo, mail, naissance, inscription, nom, prenom, residence, mot_de_passe) VALUES ('$pseudo', '$email', '$naissance', '$d_inscription', '$nom', '$prenom','$residence', '$mdp');";	//requêt SQL pour inscrire une personne dans la base de donnée
+					$result  =mysqli_query($base,$sql);	//envoie de la requête de connexion et de la requête d'insertion à MySQL
 			
-			mysqli_close($base);	
-			//header("Location: index.php");	
+					if(!$result) //si la requête échoue
+					{	
+				 		echo("Error description : ".mysqli_error($base));	//on echo la raison de l'échec de la requête
+					}	
+			
+					mysqli_close($base); //si la requête à fonctionné, alors la personne est insérée dans la base de donnée et on peut clore la connexion à MySQL
+					//header("Location: index.php");
+				}
+				else
+				{
+					echo "Erreur : Les mots de passe ne sont pas identiques."; //echo de l'erreur si les mots de passe ne sont pas identique
+				}
+			}
+			else
+			{
+				echo "Erreur : Veuillez saisir un mot de passe d'au moins 6 caractères."; //echo de l'erreur lorsque le mdp saisi est inférieur à 6caractères
+			}
 		}
 	} 
 ?>
@@ -124,15 +150,6 @@
 				<input type="text" name="pseudo" value="<?php echo !empty($pseudo)?$pseudo:'';?>">
 				<?php if (!empty($pseudoErreur)):
 				    echo $pseudoErreur;
-					endif; 
-				?>
-			</div>
-			
-			<div <?php echo !empty($mdpErreur)?'erreur':'';?>>
-				<label for="mdp">Mot de Passe :</label>
-				<input type="password" name="mdp" value="">
-				<?php if (!empty($mdpErreur)):
-				    echo $mdpErreur;
 					endif; 
 				?>
 			</div>
@@ -197,6 +214,24 @@
 				<input type="text" name="residence" value="<?php echo !empty($residence)?$residence:'';?>">
 				<?php if (!empty($residenceErreur)):
 					echo $residenceErreur;
+					endif; 
+				?>
+			</div>
+			
+			<div <?php echo !empty($mdpErreur)?'erreur':'';?>>
+				<label for="mdp">Mot de Passe :</label>
+				<input type="password" name="mdp" value="">
+				<?php if (!empty($mdpErreur)):
+				    echo $mdpErreur;
+					endif; 
+				?>
+			</div>
+			
+			<div <?php echo !empty($repeatmdpErreur)?'erreur':'';?>>
+				<label for="mdp">Répéter votre Mot de Passe :</label>
+				<input type="password" name="repeatmdp" value="">
+				<?php if (!empty($repeatmdpErreur)):
+				    echo $repeatmdpErreur;
 					endif; 
 				?>
 			</div>
