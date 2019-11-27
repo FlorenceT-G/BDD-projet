6aeda5392cd01a703cbf609d1aa2ad40129ec50a
FlorenceT-G <57944788+FlorenceT-G@users.noreply.github.com>
2019-11-27 12:46:34 +0100

    Update signaler.php

--- signaler.php
+++ signaler.php
@@ -2,51 +2,56 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+	
 	include 'myparam.inc.php';
-
-	if ( !empty($_POST)) {
-		// keep track validation errors
-		$especeError = null;
-		$localisationError = null;
-		$dateError = null;
-
-		// keep track post values
-		// $pseudo = $_POST['pseudo'];
-		$espece = $_POST['espece'];
-		$date = strtotime($_POST['date']);
-		$localisation = $_POST['localisation'];
-
-
-		// validate input
-		$valid = true;
-		if (empty($espece)) {
-			$especeError = "S'il vous plaît, sélectionnez le nom de l'espèce à signaler";
-			$valid = false;
-		}
-
-		if (empty($localisation)) {
-			$localisationError = "S'il vous plaît, saisissez la localisation de votre signalement";
-			$valid = false;
-		}
-
-		if (empty($date)) {
-			$dateError = "S'il vous plaît, saisissez la date à laquelle vous avez observé l'espèce nuisible";
-			$valid = false;
-		}
-
-		// insert data
-		if ($valid) {
-			$pseudo = $_SESSION['pseudo'];
-			$base = mysqli_connect (MYHOST, MYUSER, MYPASS, DBNAME);
-			$sql = 	"INSERT INTO `recensement`(`pseudo`, `nomverna`, `_date`, `lieu`) VALUES ('$pseudo', '$espece','$date', '$localisation')";
-			$result=mysqli_query ($base,$sql);
-			if(!$result){
- 							 echo("Error description: " . mysqli_error($base));
+	session_start();
+	if(isset($_SESSION['pseudo']))
+	{
+		$pseudo = $_SESSION['pseudo'];
+		if ( !empty($_POST)) 
+		{
+			// keep track validation errors
+			$especeError = null;
+			$localisationError = null;
+			$dateError = null;
+			// keep track post values
+			// $pseudo = $_POST['pseudo'];
+			$espece = $_POST['espece'];
+			$date = strtotime($_POST['date']);
+			$localisation = $_POST['localisation'];
+			// validate input
+			$valid = true;
+			
+			if (empty($espece)) 
+			{
+				$especeError = "S'il vous plaît, sélectionnez le nom de l'espèce à signaler";
+				$valid = false;
 			}
-			mysqli_close($base);
-
-			//header("Location: index.php");
+			if (empty($localisation)) 
+			{
+				$localisationError = "S'il vous plaît, saisissez la localisation de votre signalement";
+				$valid = false;
+			}
+			if (empty($date)) 
+			{
+				$dateError = "S'il vous plaît, saisissez la date à laquelle vous avez observé l'espèce nuisible";
+				$valid = false;
+			}
+			// insert data
+			if ($valid) 
+			{
+				$date = date("Y-m-d", $date);
+				$base = mysqli_connect(MYHOST, MYUSER, MYPASS, DBNAME);
+				$sql = 	"INSERT INTO `recensement`(`pseudo`, `nomverna`, `_date`, `lieu`) VALUES ('$pseudo', '".$espece."','$date', '$localisation')";
+				$result = mysqli_query($base,$sql);
+				
+				if(!$result)
+				{
+ 					echo("Error description: ".mysqli_error($base));
+				}
+				mysqli_close($base);
+				header("Location: accueil.php");
+			}
 		}
 	}
 ?>
@@ -64,78 +69,64 @@ error_reporting(E_ALL);
 
 <body>
     <div class="container">
-
-    			
-		    			<h2>Faire le signalement d'une espèce nuisible</h2>
+    	<?php echo "Hello, $pseudo"; ?>
+		<h2>Faire le signalement d'une espèce nuisible</h2>
 
 <!-- formulaire, utilise le php du nom "signaler.php (ce fichier)"-->
-	    			<form class="formulaire" action="signaler.php" method="post">
-
-					  <div class="control-group <?php echo !empty($Error)?'error':'';?>">
-
-					    <label class="control-label">Nom de l'espèce nuisible</label>
-
+	    <form class="formulaire" action="signaler.php" method="post">
+			<div class="control-group <?php echo !empty($Error)?'error':'';?>">
+			    <label class="control-label">Nom de l'espèce nuisible</label>
 					    <div>
 					    	
-					    	<SELECT name="espece" value="<?php echo !empty($espece)?$espece:'';?>">
-					    	<?php
-					    		$base = mysqli_connect (MYHOST, MYUSER, MYPASS, DBNAME); 
-					    		$sql = 	"SELECT nomverna FROM especes" ;
-					    		$result=mysqli_query ($base,$sql);
-
-								while($row = mysqli_fetch_array($result)) {
-									$espece= $row['nomverna'];
+					    		<SELECT name="espece" value="<?php echo !empty($espece)?$espece:'';?>">
+					    			<?php
+					    				$base = mysqli_connect (MYHOST, MYUSER, MYPASS, DBNAME); 
+					    				$sql = 	"SELECT nomverna FROM especes" ;
+					    				$result = mysqli_query($base,$sql);
+										while($row = mysqli_fetch_array($result)) 
+										{
+											$espece= $row['nomverna'];
 									
-									echo "<OPTION value=$espece> $espece </OPTION> \n"; 
-								}
-							?>
-							</SELECT>	
+											echo "<OPTION value='$espece'> $espece </OPTION> \n"; 
+										}
+								
+										mysqli_close($base);
+									?>
+								</SELECT>	
 
-					      	<?php if (!empty($especeError)): ?>
+					      		<?php if (!empty($especeError)): ?>
 
 					      		<span class="help-inline"> <?php echo $especeError;?> </span>
 
-					      	<?php endif; ?>
+					      		<?php endif; ?>
 
 					    </div>
-					  </div>
+			</div>
 
-					  <div class="control-group <?php echo !empty($localisationError)?'error':'';?>">
+			<div class="control-group <?php echo !empty($localisationError)?'error':'';?>">
+				<label class="control-label">Localisation</label>
+					<div>
+						<input  class="saisietxt" name="localisation" type="text" placeholder="Saisir une localisation" value="<?php echo !empty($localisation)?$localisation:'';?>">
+					    <?php if (!empty($localisationError)): ?>
+							<span class="help-inline"><?php echo $localisationError;?></span>
+						<?php endif;?>
 
-					    <label class="control-label">Localisation</label>
-
-					    <div>
-
-					      	<input  class="saisietxt" name="localisation" type="text" placeholder="Saisir une localisation" value="<?php echo !empty($localisation)?$localisation:'';?>">
-
-					      	<?php if (!empty($localisationError)): ?>
-
-					      		<span class="help-inline"><?php echo $localisationError;?></span>
-
-					      	<?php endif;?>
-
-					    </div>
-					  </div>
-					  <div class="control-group <?php echo !empty($dateError)?'error':'';?>">
-					    <label class="control-label">Date</label>
-					    <div>
-					      	<input  class="saisietxt" name="date" type="date"  placeholder="Saisir une date" value="<?php echo !empty($date)?$date:'';?>">
-					      	<?php if (!empty($dateError)): ?>
-					      		<span class="help-inline"><?php echo $dateError;?></span>
-					      	<?php endif;?>
-					    </div>
-					  </div>
-					  <div class="form-actions">
-
-<!--Bouton "soumettre" -->
-
-						  <button type="submit" class="button_co">Soumettre le signalement</button>
-
-						</div>
-					</form>
-				
-
-    </div> <!-- /container -->
-  </body>
+					</div>
+			</div>
+			<div class="control-group <?php echo !empty($dateError)?'error':'';?>">
+				<label class="control-label">Date</label>
+					<div>
+						<input  class="saisietxt" name="date" type="date"  placeholder="Saisir une date" value="<?php echo !empty($date)?$date:'';?>">
+						<?php if (!empty($dateError)): ?>
+							<span class="help-inline"><?php echo $dateError;?></span>
+						<?php endif;?>
+					</div>
+			</div>
+			<div class="form-actions">
+			<!--Bouton "soumettre" -->
+				<button type="submit" class="button_co">Soumettre le signalement</button>
+			</div>
+		</form>
+	</div> <!-- /container -->
+</body>
 </html>
-
