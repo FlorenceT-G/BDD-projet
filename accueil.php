cf4c5aedb5995d6b04be40d56ff4ba4ea0d83d44
FlorenceT-G <57944788+FlorenceT-G@users.noreply.github.com>
2019-11-28 11:55:39 +0100

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -68,7 +68,7 @@
 			Pour consulter les différentes signalisations déjà effectuées, pas besoin de t'inscrire, tu peux directement accéder à la page <a href="MenuConsultation">Consultation</a> et effectuer une recherche selon l'espèce ou la localisation désirée.
 		</li>
 	    <li>
-	    	Pour pourvoir soumettre un signalement, il faut que tu te crées un profil, et que tu te connectes dessus. Ensuite, rendez-vous sur la page <a href="MenuSignalement">Signalement</a>
+	    	Pour pourvoir soumettre un signalement, il faut que tu te crées un profil, et que tu te connectes dessus. Ensuite, rendez-vous sur la page <a href="MenuSignalement">Signalement</a>.
 	    </li>
 	    <li>
 	    	Pour que l'expérience des inscrits soit plus sympathique, un système de <a href = "ClassementUtilisateur"> classement </a> existe : signale toutes les espèces nuisibles que tu vois pour te hisser en haut du podium !
