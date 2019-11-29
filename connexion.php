87f98043595085896a4bb2d343f76d96d40e749f
FlorenceT-G <57944788+FlorenceT-G@users.noreply.github.com>
2019-11-29 14:41:13 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,20 +1,16 @@
 <?php
-
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
-
 	include_once("myparam.inc.php");
+	$_SESSION = null;
 	if(!empty($_POST))
 	{
 		$pseudoError = null;
 		$mdpError = null;
-
 		$pseudo = $_POST['pseudo'];
 		$mdp = $_POST['mdp'];
-
 		$valid = true;
-
 		if(empty($pseudo))
 		{
 			$pseudoErreur = 'Veuillez entrer un pseudo.';
@@ -25,14 +21,12 @@
 			$mdpErreur = 'Veuillez entrer un pseudo.';
 			$valid = false;
 		}
-
 		if($valid)
 		{
 			$mdp = md5($mdp);
 			$connect = mysqli_connect(MYHOST, MYUSER, MYPASS, DBNAME) or die ("Problème de connexion à la base de données");
 			$sql = "SELECT pseudo FROM personne WHERE pseudo = '".$pseudo."' AND mot_de_passe = '".$mdp."';";
 			$requete = mysqli_query($connect,$sql);
-
 			if(mysqli_num_rows($requete) == 0)
 			{
 				echo "Le pseudo ou le mot de passe que vous avez entrés sont erronnés ou n'existent pas dans la base de données. Veuillez recommencer ou vous inscrire.";
@@ -49,16 +43,12 @@
 			}
 		}
 	}
-
-
 // connexion à la base de données : myparam.inc.php
 //
 // $requete = mysqli_query($connect, "SELECT * FROM personne WHERE pseudo = '".pseudo."' AND mot_de_passe = '".$mdp."';");
-
 // si mysqli_num_rows($connect) != 0) // mysqli_PHP/index.php_num_rows : compte le nombre de résultat de la requête. 1 s'il y a un résultat
 // stocker les variables siimples dans des variables de sessions qui sera valable sur toutes les pages du site $_SESSION['pseudo'] = $pseudo;
 // header('Location: ./acceP_PHP/index.phpuil.php'); // permet de rediriger l'utilisateur vers la page d'acceuil une fois connecté/inscrit
-
 ?>
 
 <!DOCTYPE html>
@@ -69,36 +59,8 @@
 		<title>Connexion</title>
 	</head>
 
-	<header>
-		<a href="accueil.php" class="retouraccueil">
-			<div class="bandeau">
-				<h1>Nuisibizzbizz</h1> <!--Titre du site-->
-			</div>
-		</a>
-		<div class="MenuHaut">
-			<ul>
-				<a href="accueil.php"><li class="onglet"> Accueil </li></a>
-				<a href="MenuSignalement"><li class="onglet"> Signaler </li></a>
-				<a href="MenuConsultation"><li class="onglet">Consulter</li></a>
-				<a href="ClassementUtilisateurs"><li class="onglet">Classement utilisateurs</li></a>
-				<a href="MenuMaladies"><li class="onglet">Maladies
-				<!-- Sous-onglets, facultatifs...
-				<ul>
-					<a href="Maladie1"><li>
-						Maladie1</li></a>
-					<a href="Maladie2"><li>Maladie2</li></a>
-					<a href="Maladie3"><li>Maladie3</li></a>
-				</ul>
-				-->
-				</li></a>
-				<a href="connexion.php"><li class="onglet">Connexion</li></a>
-				<a href="inscription.php"><li class="onglet">Inscription</li></a>
-				<a href="Pagesupprcompte"><li class="onglet">Supprimer son compte</li></a>
-			</ul>
-		</div>
-		<img class="bandeau" src="fox4.jpg">
-	</header>	
-		
+	<?php include "header.php"; ?>
+
 	<body>
 	<h2>Connexion</h2>	
 	<form action="#" method="post">
@@ -123,19 +85,6 @@
 		</form>
 	</body>
 
-	<footer>
-		<p class="disclaimer"> Nous ne sommes pas responsables de la véracité des informations saisies par les utilisateurs </p>
-			<p> Nous contacter par mail: </p>
-			<div>
-				<ul>
-					<li class="contacts">
-						<address>  <a href="mailto:sarah.bonnet1@etu.univ-nantes.fr"> Sarah </a> </address>
-					</li>
-					<li  class="contacts">
-						<address>  <a href="mailto:florence.thomas1@etu.univ-nantes"> Florence </a> </address>
-					</li>
-				</ul>
-			</div>
-		<p class="merci"> Merci de votre visite ! </p> 
-	</footer>
+	<?php include "footer.php"; ?>
+
 </html>
